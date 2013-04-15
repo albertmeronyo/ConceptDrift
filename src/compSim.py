aba0fee45efed3e830220535d1a7011a4904d1fa
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2013-04-15 16:24:55 +0200

    Normalized compression distance

--- src/compSim.py
+++ src/compSim.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from __future__ import division
 import os
 import gzip
 
@@ -22,6 +23,14 @@ for i in os.listdir(dataDir):
         if i == j:
             row.append('x')
             continue
+        fj = open(os.path.join(dataDir, j), 'r')
+        fjComp = gzip.open(os.path.join(tmpDir, j + '.gz'), 'wb')
+        fjComp.writelines(fj)
+        fjComp.close()
+        fj.close()
+        fjComp = open(os.path.join(tmpDir, j + '.gz'), 'r')
+        fjCompSize = len(fjComp.read())
+        fjComp.close()
         fi = open(os.path.join(dataDir, i), 'r')
         fj = open(os.path.join(dataDir, j), 'r')
         fijComp = gzip.open(os.path.join(tmpDir, i + j + '.gz'), 'wb')
@@ -33,7 +42,7 @@ for i in os.listdir(dataDir):
         fijComp = open(os.path.join(tmpDir, i + j + '.gz'), 'r')
         fijCompSize = len(fijComp.read())
         fijComp.close()
-        row.append(fijCompSize - fiCompSize)
+        row.append(max((fijCompSize - fjCompSize)/fiCompSize, (fijCompSize - fiCompSize)/fjCompSize))
     res.append(row)
 
 for row in res:
