d0e2ac5eaac5bb60d1104ba3461da1171d749d93
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2013-04-19 12:51:16 +0200

    First concept to concept mapping implementation

--- src/compSim2.py
+++ src/compSim2.py
@@ -4,18 +4,21 @@
 from __future__ import division
 import os
 import gzip
+from SPARQLWrapper import SPARQLWrapper, JSON
+import hashlib
 
-dataDir = '../data/ecgo-sum'
-tmpDir = '../tmp'
+tmpDir = '../tmp/'
 res = []
 
-sparql = SPARQLWrapper("http://kizokumaru.dyndns.org:3030/sparql")
+sparql = SPARQLWrapper("http://kizokumaru.dyndns.org:3030/cedar/sparql")
+sparql.setReturnFormat(JSON)
+
 sparql.setQuery("""
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 PREFIX d2s: <http://www.data2semantics.org/core/> 
 
-SELECT ?label
+SELECT ?value ?label
 FROM <http://www.cedar-project.nl/data/1859>
 WHERE {
 ?s a skos:Concept ;
@@ -24,45 +27,71 @@ d2s:value ?value .
 }
 """)
 
-sparql.setReturnFormat(JSON)
-sparqlResults = sparql.query().convert()
+print "SPARQLing for source concepts..."
+sparqlResultsA = sparql.query().convert()
 
-for i in sorted(os.listdir(dataDir)):
-    fi = open(os.path.join(dataDir, i), 'r')
-    fiComp = gzip.open(os.path.join(tmpDir, i + '.gz'), 'wb')
+sparql.setQuery("""
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+PREFIX d2s: <http://www.data2semantics.org/core/> 
+
+SELECT ?value ?label
+FROM <http://www.cedar-project.nl/data/1889>
+WHERE {
+?s a skos:Concept ;
+d2s:value ?value .
+?value skos:prefLabel ?label .
+}
+""")
+
+print "SPARQLing for destination concepts..."
+sparqlResultsB = sparql.query().convert()
+
+print "Computing distances..."
+for i in sparqlResultsA["results"]["bindings"]:
+    query = """
+            SELECT ?p ?o
+            FROM <http://www.cedar-project.nl/data/1889>
+            WHERE {
+              <""" + i["value"]["value"] +  """> ?p ?o .
+            }
+            """
+    sparql.setQuery(query)
+    fi = sparql.query().convert()
+    fiID = hashlib.md5(i["value"]["value"]).hexdigest()
+    fiComp = gzip.open(os.path.join(tmpDir, fiID + '.gz'), 'wb')
     fiComp.writelines(fi)
     fiComp.close()
-    fi.close()
-    fiComp = open(os.path.join(tmpDir, i + '.gz'), 'r')
+    fiComp = open(os.path.join(tmpDir, fiID + '.gz'), 'r')
     fiCompSize = len(fiComp.read())
     fiComp.close()
     row = []
-    for j in sorted(os.listdir(dataDir)):
-        if i == j:
-            row.append('x')
-            continue
-        fj = open(os.path.join(dataDir, j), 'r')
-        fjComp = gzip.open(os.path.join(tmpDir, j + '.gz'), 'wb')
+    for j in sparqlResultsB["results"]["bindings"]:
+        query = """
+                SELECT ?p ?o
+                FROM <http://www.cedar-project.nl/data/1889>
+                WHERE {
+                <""" + i["value"]["value"] +  """> ?p ?o .
+                }
+                """
+        sparql.setQuery(query)
+        fj = sparql.query().convert()
+        fjID = hashlib.md5(j["value"]["value"]).hexdigest()
+        fjComp = gzip.open(os.path.join(tmpDir, fjID + '.gz'), 'wb')
         fjComp.writelines(fj)
         fjComp.close()
-        fj.close()
-        fjComp = open(os.path.join(tmpDir, j + '.gz'), 'r')
+        fjComp = open(os.path.join(tmpDir, fjID + '.gz'), 'r')
         fjCompSize = len(fjComp.read())
         fjComp.close()
-        fi = open(os.path.join(dataDir, i), 'r')
-        fj = open(os.path.join(dataDir, j), 'r')
-        fijComp = gzip.open(os.path.join(tmpDir, i + j + '.gz'), 'wb')
-        fijComp.write(fi.read())
-        fijComp.write(fj.read())
+        fijComp = gzip.open(os.path.join(tmpDir, fiID + fjID + '.gz'), 'wb')
+        fijComp.writelines(fi)
+        fijComp.writelines(fj)
         fijComp.close()
-        fi.close()
-        fj.close()
-        fijComp = open(os.path.join(tmpDir, i + j + '.gz'), 'r')
+        fijComp = open(os.path.join(tmpDir, fiID + fjID + '.gz'), 'r')
         fijCompSize = len(fijComp.read())
         fijComp.close()
         row.append(max((fijCompSize - fjCompSize)/fiCompSize, (fijCompSize - fiCompSize)/fjCompSize))
     res.append(row)
 
-print sorted(os.listdir(dataDir))
 for row in res:
     print row
