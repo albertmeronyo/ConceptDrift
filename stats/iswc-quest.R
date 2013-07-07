9cb3d645710bd54be346a4d1452962f3965c320a
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2013-07-07 22:07:31 +0200

    Normalization of occupations using HISCO

--- stats/iswc-quest.R
+++ stats/iswc-quest.R
@@ -1,4 +1,5 @@
 library(SPARQL)
+library(gdata)
 
 ##########################
 # Retrieve data via SPARQL
@@ -15,39 +16,79 @@ sparql_prefix <- "PREFIX d2s: <http://www.data2semantics.org/core/>
 q <- paste(sparql_prefix,"SELECT DISTINCT (str(?age_s) AS ?age_c) (str(?gender_s) AS ?gender_c) (str(?marital_status_s) AS ?marital_status_c) (str(?municipality_s) AS ?municipality_c) (str(?occ_class_s) AS ?occ_class_c) (str(?occ_subclass_s) AS ?occ_subclass_c) (str(?occupation_s) AS ?occupation_r) (REPLACE(?position_s, \"^ +| +$\", \"\") AS ?position_c) ?population
 FROM <http://lod.cedar-project.nl/resource/BRT_1889_08_T1>
 WHERE {
- ?cell d2s:isObservation [ d2s:dimension ?gender ;
-                           d2s:dimension ?marital_status ;
-                           d2s:dimension ?age ;
-  		   ns1:BENAMING_van_de_onderdeelen_der_onderscheidene_beroepsklassen__met_de_daartoe_behoordende_beroepen ?occupation ;
-			   ns1:Positie_in_het_beroep__aangeduid_met_A__B__C_of_D_ ?position ;
-       			   d2s:populationSize ?population ] .
- ?occupation skos:broader ?occ_subclass .
- ?occ_subclass skos:broader ?occ_class .
- ?occ_class skos:broader ?municipality .
- ?municipality skos:prefLabel ?municipality_s .
- ?occ_class skos:prefLabel ?occ_class_s .
- ?occ_subclass skos:prefLabel ?occ_subclass_s .
- ?cell d2s:cell ?cell_s .
- ?age skos:prefLabel ?age_s .
- ?gender skos:prefLabel ?gender_s .
- ?marital_status skos:prefLabel ?marital_status_s .
- ?occupation skos:prefLabel ?occupation_s .
- ?position skos:prefLabel ?position_s .
- FILTER (?gender IN (cd:M, cd:V))
- FILTER (?marital_status IN (cd:O, cd:G))
- FILTER (?age IN (cd:12___1878, cd:13___1876, cd:14_---_15__1875___---_1874, cd:16_---_17__1873___---_1872, cd:1878_en_later__beneden_12_j_, cd:18_---_22__1871___---_1867, cd:Geboortejaren___leeftijd_in_j_, cd:25_---_35__1864___---_1854, cd:36_---_50__1853___---_1839, cd:51_---_60__1838___---_1829, cd:61_---_65__1828___---_1824, cd:66_---_70__1823_---_1818, cd:71_en_daarboven__1818_en_vroeger, cd:Van_onbekenden_leeftijd, cd:23_---_24__1866___---_1865))
-} ORDER BY (?cell)")
+           ?cell d2s:isObservation [ d2s:dimension ?gender ;
+           d2s:dimension ?marital_status ;
+           d2s:dimension ?age ;
+           ns1:BENAMING_van_de_onderdeelen_der_onderscheidene_beroepsklassen__met_de_daartoe_behoordende_beroepen ?occupation ;
+           d2s:populationSize ?population ] .
+           OPTIONAL {
+           ?cell d2s:isObservation [ns1:Positie_in_het_beroep__aangeduid_met_A__B__C_of_D_ ?position] .
+           ?position skos:prefLabel ?position_s .
+           }
+           
+           ?occupation skos:broader ?occ_subclass .
+           ?occ_subclass skos:broader ?occ_class .
+           ?occ_class skos:broader ?municipality .
+           ?municipality skos:prefLabel ?municipality_s .
+           ?occ_class skos:prefLabel ?occ_class_s .
+           ?occ_subclass skos:prefLabel ?occ_subclass_s .
+           ?cell d2s:cell ?cell_s .
+           ?age skos:prefLabel ?age_s .
+           ?gender skos:prefLabel ?gender_s .
+           ?marital_status skos:prefLabel ?marital_status_s .
+           ?occupation skos:prefLabel ?occupation_s .
+           
+           FILTER (?gender IN (cd:M, cd:V))
+           FILTER (?marital_status IN (cd:O, cd:G))
+           FILTER (?age IN (cd:12___1878, cd:13___1876, cd:14_---_15__1875___---_1874, cd:16_---_17__1873___---_1872, cd:1878_en_later__beneden_12_j_, cd:18_---_22__1871___---_1867, cd:Geboortejaren___leeftijd_in_j_, cd:25_---_35__1864___---_1854, cd:36_---_50__1853___---_1839, cd:51_---_60__1838___---_1829, cd:61_---_65__1828___---_1824, cd:66_---_70__1823_---_1818, cd:71_en_daarboven__1818_en_vroeger, cd:Van_onbekenden_leeftijd, cd:23_---_24__1866___---_1865))
+           } ORDER BY (?cell)")
 
 res <- SPARQL(endpoint,q,ns=prefix,extra=options)$results
+
+sparql_prefix2 <- "PREFIX d2s: <http://www.data2semantics.org/core/>
+PREFIX cd: <http://www.data2semantics.org/data/BRT_1899_04_T_marked/Noordholland/>
+PREFIX ns1: <http://www.data2semantics.org/core/Noordholland/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>"
+
+q2 <- paste(sparql_prefix2,
+            "SELECT DISTINCT (str(?age_s) AS ?age_c) (str(?gender_s) AS ?gender_c) (str(?marital_status_s) AS ?marital_status_c) (str(?municipality_s) AS ?municipality_c) (str(?occupation_s) AS ?occupation_c) (str(?position_s) AS ?position_c) ?population
+FROM <http://lod.cedar-project.nl/resource/BRT_1899_04_T>
+WHERE {
+            ?cell d2s:isObservation [ d2s:dimension ?gender ;
+            d2s:dimension ?marital_status ;
+            d2s:dimension ?age ;
+            ns1:Benaming_van_de_onderdeelen_der_onderscheidene_beroepsklassen__met_de_daartoe_behoorende_beroepen ?occupation ;
+            ns1:Gemeenten ?municipality ;
+            d2s:populationSize ?population ] .
+            OPTIONAL {
+            ?cell d2s:isObservation [ ns1:Positie_in_het_beroep ?position ] .
+            ?position skos:prefLabel ?position_s .
+            }
+            
+            ?cell d2s:cell ?cell_s .
+            ?age skos:prefLabel ?age_s .
+            ?gender skos:prefLabel ?gender_s .
+            ?marital_status skos:prefLabel ?marital_status_s .
+            ?occupation skos:prefLabel ?occupation_s .
+            ?municipality skos:prefLabel ?municipality_s .
+            
+            FILTER (?gender IN (cd:MANNEN, cd:VROUWEN))
+            FILTER (?marital_status IN (cd:O_, cd:G_))
+            FILTER (?age IN (cd:12_of_13_1887_-_1886, cd:14_of_15_1885_-_1884, cd:16_of_17_1883_-_1882, cd:18_-_22_1881_-_1877, cd:23_-_35_1876_-_1864, cd:beneden_12_jaar_1888_en_later, cd:51_-_60_1848_-_1839, cd:61_-_65_1838_-_1834, cd:66_-_70_1833_-_1829, cd:71_en_daarboven_1828_en_vroeger, cd:36_-_50_1863_-_1849))
+            } ORDER BY (?cell)") 
+
+res2 <- SPARQL(endpoint,q2,ns=prefix,extra=options)$results
 
 #############
 # Pre-process
 #############
-# Data.frame removing all totals
-df <- data.frame(res[-grep("(Totaal|totaal)", res$occupation_r),])
+# Data.frame for first dataset, removing all totals
+df <- data.frame(res[-grep("(Totaal|totaal)", res$occupation_c),])
 # Remove all rows with NAs
 df <- df[complete.cases(df),]
-# Column data types
+# Remove possibly duplicated rows
+df <- df[!duplicated(df),]
+# Assign column data types
 df$age_c <- as.factor(df$age_c)
 df$gender_c <- as.factor(df$gender_c)
 df$marital_status_c <- as.factor(df$marital_status_c)
@@ -57,6 +98,41 @@ df$occ_subclass_c <- as.factor(df$occ_subclass_c)
 df$occupation_c <- as.factor(df$occupation_c)
 df$position_c <- as.factor(df$position_c)
 df$population <- as.numeric(df$population)
+# Load HISCO data and merge
+h1 <- read.xls('/Users/Albert/Downloads/index/1889_08_T1.xls')
+h1$ID <- NULL
+h1$ALTBEROEP <- NULL 
+h1$STATUS <- NULL 
+h1$REL <- NULL
+h1 <- h1[complete.cases(h1),]
+df.hisco <- merge(df, h1, by.y=c("BEROEP","POSITIE"), by.x=c("occupation_c","position_c"))
+df.hisco <- df.hisco[!duplicated(df.hisco),]
+
+# Data.frame for second dataset, removing all totals
+df2 <- data.frame(res2[-grep("(Totaal|totaal)", res2$occupation_c),])
+# We also remove the totals for the whole province ("Geheele Provincie")
+df2 <- data.frame(df2[-grep("Geheele", df2$municipality_c),])
+# (OPTIONAL) Remove all rows with non-identified municipalities ("Gemeentes met...")
+df2 <- data.frame(df2[-grep("Gemeenten met", df2$municipality_c),])
+# Remove all rows with NAs
+df2 <- df2[complete.cases(df2),]
+# Assign column data types
+df2$age_c <- as.factor(df2$age_c)
+df2$gender_c <- as.factor(df2$gender_c)
+df2$marital_status_c <- as.factor(df2$marital_status_c)
+df2$municipality_c <- as.factor(df2$municipality_c)
+df2$occupation_c <- as.factor(df2$occupation_c)
+df2$position_c <- as.factor(df2$position_c)
+df2$population <- as.numeric(df2$population)
+# Load HISCO data and merge
+h2 <- read.xls('/Users/Albert/Downloads/index2/1899_04_T.xls')
+h2$ID <- NULL
+h2$ALTBEROEP <- NULL 
+h2$STATUS <- NULL 
+h2$REL <- NULL
+h2 <- h2[complete.cases(h2),]
+df2.hisco <- merge(df2, h2, by.y=c("BEROEP","POSITIE"), by.x=c("occupation_c","position_c"))
+df2.hisco <- df2.hisco[!duplicated(df2.hisco),]
 
 # Do variable pairings
 
