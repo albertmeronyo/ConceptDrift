32490066456a4da4484a4453db88fe7bea178d74
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2014-02-12 16:57:34 +0100

    GODVERDOMME

--- stats/ldsem-change.R
+++ stats/ldsem-change.R
@@ -1,175 +1,108 @@
-###########################
-# Load CSV with SPARQL dump
-###########################
-res <- read.csv('/home/amp/src/ConceptDrift/stats/data/BRT_1889_08_T1.csv', header = TRUE)
-res2 <- read.csv('/home/amp/src/ConceptDrift/stats/data/BRT_1899_04_T.csv', header = TRUE)
+library(SPARQL)
+
+########################################
+# Querying data from 1849, 1859 and 1889
+########################################
+
+endpoint <- "http://lod.cedar-project.nl:8080/sparql/cedar"
+options <- NULL
+prefix <- c()
+sparql_prefix <- "PREFIX d2s: <http://lod.cedar-project.nl/core/>
+PREFIX ns1: <http://lod.cedar-project.nl/core/DERDE_GEDEELTE/>
+PREFIX cd: <http://lod.cedar-project.nl/resource/VT_1849_07_H3/DERDE_GEDEELTE/>"
+q <- paste(sparql_prefix,"SELECT DISTINCT (str(?gender_s) AS ?gender_c) (str(?age_s) AS ?age_c) (str(?occupation_s) AS ?occupation_c) (str(?place_s) AS ?place_c) ?population
+FROM <http://lod.cedar-project.nl/resource/VT_1849_07_H3>
+WHERE {
+?cell d2s:isObservation [ d2s:dimension ?gender ;
+      d2s:dimension ?age ;
+      ns1:Beroepen_of_middelen_van_bestaan ?occupation ;
+      ns1:Gebied ?place ;
+      d2s:populationSize ?population ] .
+
+      ?gender skos:prefLabel ?gender_s .
+      ?age skos:prefLabel ?age_s .
+      ?occupation skos:prefLabel ?occupation_s .
+      ?place skos:prefLabel ?place_s .
+
+      FILTER (?gender IN (cd:Mannelijk, cd:Vrouwelijk))
+      FILTER (?age IN (cd:Beneden_de_10_jaren, cd:Van_10_en_11_jaren, cd:Van_12_tot_en_met_15_jaren, cd:Van_16_jaren_en_daarboven, cd:TOTAAL))
+}")
+
+sparql_prefix2 <- "PREFIX d2s: <http://www.data2semantics.org/core/>
+PREFIX ns3: <http://www.data2semantics.org/core/het_Rijk/>
+PREFIX cd: <http://www.data2semantics.org/data/VT_1859_02_H2_marked/het_Rijk/>"
+q2 <- paste(sparql_prefix2,"SELECT DISTINCT (str(?gender_s) AS ?gender_c) (str(?factual_s) AS ?factual_c) (str(?age_s) AS ?age_c) (str(?occupation_s) AS ?occupation_c) (str(?class_s) AS ?class_c) ?population
+FROM <http://lod.cedar-project.nl/resource/VT_1859_02_H2>
+WHERE {
+?cell d2s:isObservation [ d2s:dimension ?gender ;
+      d2s:dimension ?factual ;
+      d2s:dimension ?age ;
+      ns3:BEROEPEN_OF_MIDDELEN_VAN_BESTAAN ?occupation ;
+      ns3:BEROEPSKLASSEN ?class ;
+      d2s:populationSize ?population ] .
+
+      ?gender skos:prefLabel ?gender_s .
+      ?factual skos:prefLabel ?factual_s .
+      ?age skos:prefLabel ?age_s .
+      ?occupation skos:prefLabel ?occupation_s .
+      ?class skos:prefLabel ?class_s .
+
+      FILTER (?gender IN (cd:M_, cd:V_))
+      FILTER (?factual IN (cd:Werkelijke_bevolking, cd:Feitelijke_bevolking))
+      FILTER (?age IN (cd:PERSONEN_BENEDEN_DE__10_JAREN_GEBOREN_IN_1859-1850, cd:PERSONEN_VAN_10_EN_11_JAREN_GEBOREN_IN_1849-1848, cd:PERSONEN_VAN_12_EN_16_JAREN_GEBOREN_IN_1847-1844, cd:PERSONEN_VAN_16_EN_23_JAREN_GEBOREN_IN_1843-1837, cd:PERSONEN_VAN_23_JAREN_EN_DAARBOVEN_GEBOREN_IN_1836_EN_VROEGERE_JAREN, cd:ALGEMEEN_TOTAAL))
+}")
+
+sparql_prefix3 <- "PREFIX d2s: <http://www.data2semantics.org/core/>
+PREFIX cd: <http://www.data2semantics.org/data/BRT_1889_02_T1_marked/Table_1/>
+PREFIX ns1: <http://www.data2semantics.org/core/Table_1/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>"
+q3 <- paste(sparql_prefix3,"SELECT DISTINCT (str(?age_s) AS ?age_c) (str(?gender_s) AS ?gender_c) (str(?marital_status_s) AS ?marital_status_c) (str(?occ_class_s) AS ?occ_class_c) (str(?occ_subclass_s) AS ?occ_subclass_c) (str(?occupation_s) AS ?occupation_c) ?occupation (REPLACE(?position_s, \"^ +| +$\", \"\") AS ?position_c) ?population
+FROM <http://lod.cedar-project.nl/resource/BRT_1889_02_T1>
+WHERE {
+           ?cell d2s:isObservation [ d2s:dimension ?gender ;
+           d2s:dimension ?marital_status ;
+           d2s:dimension ?age ;
+           ns1:BENAMING_van_de_onderdeelen_der_onderscheidene_beroepsklassen__met_de_daartoe_behoordende_beroepen ?occupation ;
+           d2s:populationSize ?population ] .
+           OPTIONAL {
+           ?cell d2s:isObservation [ns1:Positie_in_het_beroep__aangeduid_met_A__B__C_of_D_ ?position] .
+           ?position skos:prefLabel ?position_s .
+           }
+           
+           ?occupation skos:broader ?occ_subclass .
+           ?occ_subclass skos:broader ?occ_class .
+           ?occ_class skos:prefLabel ?occ_class_s .
+           ?occ_subclass skos:prefLabel ?occ_subclass_s .
+           ?cell d2s:cell ?cell_s .
+           ?age skos:prefLabel ?age_s .
+           ?gender skos:prefLabel ?gender_s .
+           ?marital_status skos:prefLabel ?marital_status_s .
+           ?occupation skos:prefLabel ?occupation_s .
+           
+           FILTER (?gender IN (cd:M, cd:V))
+           FILTER (?marital_status IN (cd:O, cd:G))
+           FILTER (?age IN (cd:12___1878, cd:13___1876, cd:14_---_15__1875___---_1874, cd:16_---_17__1873___---_1872, cd:1878_en_later__beneden_12_j_, cd:18_---_22__1871___---_1867, cd:Geboortejaren___leeftijd_in_j_, cd:25_---_35__1864___---_1854, cd:36_---_50__1853___---_1839, cd:51_---_60__1838___---_1829, cd:61_---_65__1828___---_1824, cd:66_---_70__1823_---_1818, cd:71_en_daarboven__1818_en_vroeger, cd:Van_onbekenden_leeftijd, cd:23_---_24__1866___---_1865))
+           } ORDER BY (?cell)")
+
+
+res <- SPARQL(endpoint,q,ns=prefix,extra=options)$results
+res2 <- SPARQL(endpoint,q2,ns=prefix,extra=options)$results
+res3 <- SPARQL(endpoint,q3,ns=prefix,extra=options)$results
 
 #############
 # Pre-process
 #############
 # Data.frame for first dataset, removing all totals
-df <- data.frame(res[-grep("(Totaal|totaal)", res$occupation_c),])
+df49 <- res
+df59 <- res2
+df89 <- res3
 # Remove all rows with NAs
-df <- df[complete.cases(df),]
+df49 <- df49[complete.cases(df49),]
+df59 <- df59[complete.cases(df59),]
+df89 <- df89[complete.cases(df89),]
 # Remove possibly duplicated rows
-df <- df[!duplicated(df),]
+df49 <- df49[!duplicated(df49),]
+df59 <- df59[!duplicated(df59),]
+df89 <- df89[!duplicated(df89),]
 # Assign column data types
-df$age_c <- as.factor(df$age_c)
-df$gender_c <- as.factor(df$gender_c)
-df$marital_status_c <- as.factor(df$marital_status_c)
-df$municipality_c <- as.factor(df$municipality_c)
-df$occ_class_c <- as.factor(df$occ_class_c)
-df$occ_subclass_c <- as.factor(df$occ_subclass_c)
-df$occupation_c <- as.factor(df$occupation_c)
-df$position_c <- as.factor(df$position_c)
-df$population <- as.numeric(df$population)
-# Load HISCO data and merge
-h1 <- read.xls('/Users/Albert/src/ConceptDrift/stats/1889_08_T1.xls')
-h1$ID <- NULL
-h1$ALTBEROEP <- NULL 
-h1$STATUS <- NULL 
-h1$REL <- NULL
-h1 <- h1[complete.cases(h1),]
-df.hisco <- merge(df, h1, by.y=c("BEROEP","POSITIE"), by.x=c("occupation_c","position_c"))
-df.hisco <- df.hisco[!duplicated(df.hisco),]
-df.hisco$HISCO <- as.factor(df.hisco$HISCO)
 
-# Data.frame for second dataset, removing all totals
-df2 <- data.frame(res2[-grep("(Totaal|totaal)", res2$occupation_c),])
-# We also remove the totals for the whole province ("Geheele Provincie")
-df2 <- data.frame(df2[-grep("Geheele", df2$municipality_c),])
-# (OPTIONAL) Remove all rows with non-identified municipalities ("Gemeenten met...")
-df2 <- data.frame(df2[-grep("Gemeenten met", df2$municipality_c),])
-# Update list of levels (we just removed some)
-df2$municipality_c <- factor(df2$municipality_c)
-# Remove all rows with NAs
-df2 <- df2[complete.cases(df2),]
-# Assign column data types
-df2$age_c <- as.factor(df2$age_c)
-df2$gender_c <- as.factor(df2$gender_c)
-df2$marital_status_c <- as.factor(df2$marital_status_c)
-df2$municipality_c <- as.factor(df2$municipality_c)
-df2$occupation_c <- as.factor(df2$occupation_c)
-df2$position_c <- as.factor(df2$position_c)
-df2$population <- as.numeric(df2$population)
-# Load HISCO data and merge
-h2 <- read.xls('/Users/Albert/src/ConceptDrift/stats/1899_04_T.xls')
-h2$ID <- NULL
-h2$ALTBEROEP <- NULL 
-h2$STATUS <- NULL 
-h2$REL <- NULL
-h2 <- h2[complete.cases(h2),]
-df2.hisco <- merge(df2, h2, by.y=c("BEROEP","POSITIE"), by.x=c("occupation_c","position_c"))
-df2.hisco <- df2.hisco[!duplicated(df2.hisco),]
-df2.hisco$HISCO <- as.factor(df2.hisco$HISCO)
-
-# Now we extract the lists of HISCO codes for both years
-hcodes <- c()
-for(hcode in levels(df.hisco$HISCO)) {
-  hcodes <- append(hcodes, hcode)
-}
-hcodes2 <- c()
-for(hcode in levels(df2.hisco$HISCO)) {
-  hcodes2 <- append(hcodes2, hcode)
-}
-# Common HISCO codes between the two years
-common.hcodes <- intersect(hcodes, hcodes2)
-
-##############################
-# Manual dataset harmonization
-##############################
-
-a <- df.hisco
-b <- df2.hisco
-
-# Removal of non-common or not useful columns
-a$occupation <- NULL
-a$occupation_c <- NULL
-a$occ_class_c <- NULL
-a$occ_subclass_c <- NULL
-
-b$occupation_c <- NULL
-b$occupation <- NULL
-
-# Position: removal of factor 'Z'
-a <- subset(a, !a$position_c %in% c('Z'))
-a$position_c <- factor(a$position_c)
-
-# Municipality
-# Source error: in a, 'X' should be 'Amsterdam' and 'III' should be 'Den Helder'
-levels(a$municipality_c) <- c('Alkmaar', 'Amsterdam', 'Edam', 'Enkhuizen', 'Haarlem', 'Haarlemmermeer', 'Hilversum', 'Hoorn', 'Den Helder', 'Nieuwer Amstel', 'Purmerend', 'Sloten', 'Texel', 'Velsen', 'Weesp', 'Amsterdam', 'Haarlem', 'Zaandam')
-# Make values consistent in b
-levels(b$municipality_c) <- c('Alkmaar', 'Amsterdam', 'Beverwijk', 'Bloemendaal', 'Bussum', 'Edam', 'Enkhuizen', 'Haarlem', 'Haarlemmermeer', 'Den Helder', 'Hilversum', 'Hoorn', 'Nieuwer Amstel', 'Purmerend', 'Sloten', 'Texel', 'Velzen', 'Weesp', 'Wormerveer', 'Zaandam')
-
-# Age
-# Age names
-levels(a$age_c) <- c('12 years', '13 years', '14 to 15 years', '16 to 17 years', '18 to 22 years', 'Less than 12 years', '23 to 24 years', '25 to 35 years', '36 to 50 years', '51 to 60 years', '61 to 65 years', '66 to 70 years', 'More than 71 years', 'Geboortejaren.  leeftijd in j.', 'Unknown age')
-levels(b$age_c) <- c('12 to 13 years', '14 to 15 years', '16 to 17 years', '18 to 22 years', '23 to 35 years', '36 to 50 years', '51 to 60 years', '61 to 65 years', '66 to 70 years', 'More than 71 years', 'Less than 12 years')
-# Age ranges: merge rows in dataframe a of 12 and 13 years, and 23-24 and 25-35
-levels(a$age_c) <- c('12 to 13 years', '12 to 13 years', '14 to 15 years', '16 to 17 years', '18 to 22 years', 'Less than 12 years', '23 to 35 years', '23 to 35 years', '36 to 50 years', '51 to 60 years', '61 to 65 years', '66 to 70 years', 'More than 71 years', 'Geboortejaren.  leeftijd in j.', 'Unknown age')
-
-# Gender / marital status
-levels(a$gender_c) <- c('Male', 'Female')
-levels(b$gender_c) <- c('Male', 'Female')
-levels(a$marital_status_c) <- c('Married', 'Unmarried')
-levels(b$marital_status_c) <- c('Married', 'Unmarried')
-
-# Aggregate all observations that share *all* factors, except maybe population
-a <- with(a, aggregate(population, by = list(position_c = position_c, age_c = age_c, gender_c = gender_c, marital_status_c = marital_status_c, municipality_c = municipality_c, HISCO = HISCO), FUN = sum))
-b <- with(b, aggregate(population, by = list(position_c = position_c, age_c = age_c, gender_c = gender_c, marital_status_c = marital_status_c, municipality_c = municipality_c, HISCO = HISCO), FUN = sum))
-colnames(a) <- c('position', 'age', 'gender', 'marital_status', 'municipality', 'hisco', 'population')
-colnames(b) <- c('position', 'age', 'gender', 'marital_status', 'municipality', 'hisco', 'population')
-
-# Now the dataset looks OK, but we remove 'Unknown age', 'Leeftijd...' and empty cells because they look as incomplete or missing data
-a <- subset(a, !a$age %in% c('Unknown age', 'Geboortejaren.  leeftijd in j.'))
-a$age <- factor(a$age)
-a <- subset(a, !a$position %in% c(''))
-a$position <- factor(a$position)
-b <- subset(b, !b$position %in% c(''))
-b$position <- factor(b$position)
-
-# HISCO code -1 means "no mapping" and -2 means "Unemployed".
-# -2 could be interesting, but -1 seems to be introducing noise and we delete it
-a <- subset(a, !a$hisco %in% c('-1'))
-a$hisco <- factor(a$hisco)
-b <- subset(b, !b$hisco %in% c('-1'))
-b$hisco <- factor(b$hisco)
-
-a$hisco <- as.factor(a$hisco)
-b$hisco <- as.factor(b$hisco)
-
-################################
-# Removal of column "population"
-################################
-
-# We use the population column to repeat the rows as many times as this column specifies
-# Then we remove the column
-a <- a[rep(1:nrow(a), a$population),]
-b <- b[rep(1:nrow(b), b$population),]
-a$population <- NULL
-b$population <- NULL
-
-# Save to CSV
-write.table(a, file='/Users/Albert/src/ConceptDrift/stats/BRT_1889_NH_pp.csv', sep=',', row.names=F)
-write.table(b, file='/Users/Albert/src/ConceptDrift/stats/BRT_1899_NH_pp.csv', sep=',', row.names=F)
-
-#####
-# MCA
-#####
-
-cats_a <- apply(a, 2, function(x) nlevels(as.factor(x)))
-cats_b <- apply(b, 2, function(x) nlevels(as.factor(x)))
-
-# MCA of the FactoMineR package
-mca1_a <- MCA(a, graph = FALSE)
-mca1_b <- MCA(b, graph = FALSE)
-
-# MCA of the MASS package
-mca2_a <- mca(a, nf = 5)
-mca2_b <- mca(b, nf = 5)
-
-# Plots
-mca2_a_vars_df <- data.frame(mca2_a$cs, Variable = rep(names(cats_a), cats_a))
-ggplot(data = mca2_a_vars_df, aes(x = X1, y = X2, label = rownames(mca2_a_vars_df))) + geom_hline(yintercept = 0, colour = "gray70") + geom_vline(xintercept = 0, colour = "gray70") + geom_text(aes(colour = Variable)) + ggtitle("MCA plot of variables using R package MASS")
-mca2_b_vars_df <- data.frame(mca2_b$cs, Variable = rep(names(cats_b), cats_b))
-ggplot(data = mca2_b_vars_df, aes(x = X1, y = X2, label = rownames(mca2_b_vars_df))) + geom_hline(yintercept = 0, colour = "gray70") + geom_vline(xintercept = 0, colour = "gray70") + geom_text(aes(colour = Variable)) + ggtitle("MCA plot of variables using R package MASS")
