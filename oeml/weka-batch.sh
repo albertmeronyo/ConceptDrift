fbfdd2ff1faabebf2db404e1e8caf6d3ed925626
Albert Meronyo-Penyuela <albert.meronyo@gmail.com>
2014-03-06 10:49:54 +0100

    Skipping previously executed classifiers

--- oeml/weka-batch.sh
+++ oeml/weka-batch.sh
@@ -8,39 +8,39 @@ WEKAPATH="-classpath weka.jar"
 echo "Starting WEKA-batch..."
 
 # cat /dev/null > $OUTPUT
-echo "Starting BayesNet..."
-java $MEM $WEKAPATH weka.classifiers.bayes.BayesNet -D -Q weka.classifiers.bayes.net.search.local.K2 -- -P 1 -S BAYES -E weka.classifiers.bayes.net.estimate.SimpleEstimator -- -A 0.5 $INPUT -i -o >> $OUTPUT
-echo "BayesNet completed."
-echo "Starting NaiveBayes..."
-java $MEM $WEKAPATH weka.classifiers.bayes.NaiveBayes $INPUT -i -o >> $OUTPUT
-echo "NaiveBayes completed."
-echo "Starting NaiveBayesMultinomial..."
-java $MEM $WEKAPATH weka.classifiers.bayes.NaiveBayesMultinomial $INPUT -i -o >> $OUTPUT
-echo "NaiveBayesMultinomial completed."
-echo "Starting NaiveBayesMultinomialUpdateable..."
-java $MEM $WEKAPATH weka.classifiers.bayes.NaiveBayesMultinomialUpdateable $INPUT -i -o >> $OUTPUT
-echo "NaiveBayesMultinomialUpdateable completed."
-echo "Starting NaiveBayesUpdateable..."
-java $MEM $WEKAPATH weka.classifiers.bayes.NaiveBayesUpdateable $INPUT -i -o >> $OUTPUT
-echo "NaiveBayesUpdateable completed."
-echo "Starting Logistic..."
-java $MEM $WEKAPATH weka.classifiers.functions.Logistic -R 1.0E-8 -M -1 $INPUT -i -o >> $OUTPUT
-echo "Logistic completed."
-echo "Starting MultilayerPerceptron..."
-java $MEM $WEKAPATH weka.classifiers.functions.MultilayerPerceptron -L 0.3 -M 0.2 -N 500 -V 0 -S 0 -E 20 -H a $INPUT -i -o >> $OUTPUT
-echo "MultilayerPerceptron completed."
-echo "Starting SGD..."
-java $MEM $WEKAPATH weka.classifiers.functions.SGD -F 0 -L 0.01 -R 1.0E-4 -E 500 -C 0.001 $INPUT -i -o >> $OUTPUT
-echo "SGD completed."
-echo "Starting SimpleLogistic..."
-java $MEM $WEKAPATH weka.classifiers.functions.SimpleLogistic -I 0 -M 500 -H 50 -W 0.0 $INPUT -i -o >> $OUTPUT
-echo "SimpleLogistic completed."
-echo "Starting SMO..."
-java $MEM $WEKAPATH weka.classifiers.functions.SMO -C 1.0 -L 0.001 -P 1.0E-12 -N 0 -V -1 -W 1 -K "weka.classifiers.functions.supportVector.PolyKernel -C 250007 -E 1.0" $INPUT -i -o >> $OUTPUT
-echo "SMO completed."
-echo "Starting VotedPerceptron..."
-java $MEM $WEKAPATH weka.classifiers.functions.VotedPerceptron -I 1 -E 1.0 -S 1 -M 10000 $INPUT -i -o >> $OUTPUT
-echo "VotedPerceptron completed."
+# echo "Starting BayesNet..."
+# java $MEM $WEKAPATH weka.classifiers.bayes.BayesNet -D -Q weka.classifiers.bayes.net.search.local.K2 -- -P 1 -S BAYES -E weka.classifiers.bayes.net.estimate.SimpleEstimator -- -A 0.5 $INPUT -i -o >> $OUTPUT
+# echo "BayesNet completed."
+# echo "Starting NaiveBayes..."
+# java $MEM $WEKAPATH weka.classifiers.bayes.NaiveBayes $INPUT -i -o >> $OUTPUT
+# echo "NaiveBayes completed."
+# echo "Starting NaiveBayesMultinomial..."
+# java $MEM $WEKAPATH weka.classifiers.bayes.NaiveBayesMultinomial $INPUT -i -o >> $OUTPUT
+# echo "NaiveBayesMultinomial completed."
+# echo "Starting NaiveBayesMultinomialUpdateable..."
+# java $MEM $WEKAPATH weka.classifiers.bayes.NaiveBayesMultinomialUpdateable $INPUT -i -o >> $OUTPUT
+# echo "NaiveBayesMultinomialUpdateable completed."
+# echo "Starting NaiveBayesUpdateable..."
+# java $MEM $WEKAPATH weka.classifiers.bayes.NaiveBayesUpdateable $INPUT -i -o >> $OUTPUT
+# echo "NaiveBayesUpdateable completed."
+# echo "Starting Logistic..."
+# java $MEM $WEKAPATH weka.classifiers.functions.Logistic -R 1.0E-8 -M -1 $INPUT -i -o >> $OUTPUT
+# echo "Logistic completed."
+# echo "Starting MultilayerPerceptron..."
+# java $MEM $WEKAPATH weka.classifiers.functions.MultilayerPerceptron -L 0.3 -M 0.2 -N 500 -V 0 -S 0 -E 20 -H a $INPUT -i -o >> $OUTPUT
+# echo "MultilayerPerceptron completed."
+# echo "Starting SGD..."
+# java $MEM $WEKAPATH weka.classifiers.functions.SGD -F 0 -L 0.01 -R 1.0E-4 -E 500 -C 0.001 $INPUT -i -o >> $OUTPUT
+# echo "SGD completed."
+# echo "Starting SimpleLogistic..."
+# java $MEM $WEKAPATH weka.classifiers.functions.SimpleLogistic -I 0 -M 500 -H 50 -W 0.0 $INPUT -i -o >> $OUTPUT
+# echo "SimpleLogistic completed."
+# echo "Starting SMO..."
+# java $MEM $WEKAPATH weka.classifiers.functions.SMO -C 1.0 -L 0.001 -P 1.0E-12 -N 0 -V -1 -W 1 -K "weka.classifiers.functions.supportVector.PolyKernel -C 250007 -E 1.0" $INPUT -i -o >> $OUTPUT
+# echo "SMO completed."
+# echo "Starting VotedPerceptron..."
+# java $MEM $WEKAPATH weka.classifiers.functions.VotedPerceptron -I 1 -E 1.0 -S 1 -M 10000 $INPUT -i -o >> $OUTPUT
+# echo "VotedPerceptron completed."
 # echo "Starting IBk..."
 # java $MEM $WEKAPATH weka.classifiers.lazy.IBk -K 1 -W 0 -A "weka.core.neighboursearch.LinearNNSearch -A \"weka.core.EuclideanDistance -R first-last\"" $INPUT -i -o >> $OUTPUT
 # echo "IBk completed."
