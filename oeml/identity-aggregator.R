77796af58222d77c07948dcdeec644c7bcf0eda5
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2014-03-12 10:42:36 +0100

    Keeping identity of training instances

---
+++ oeml/identity-aggregator.R
@@ -0,0 +1,8 @@
+##########################################################
+# Pastes columns to preserve identity of instances in OEML
+##########################################################
+
+df.a <- read.csv('/Users/Albert/src/ConceptDrift/oeml/data/feats_3.5.1.csv', header=F)
+df.b <- read.csv('/Users/Albert/src/ConceptDrift/oeml/data/feats_3.6.csv', header=F)
+df.c <- read.csv('/Users/Albert/src/ConceptDrift/oeml/data/feats_3.7.csv', header=F)
+
