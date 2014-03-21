69bf1b93fffc0e2a2f7776009249a832a8c999c5
Albert Meronyo-Penyuela <albert.meronyo@gmail.com>
2014-03-21 12:11:56 +0100

    Redirected error to out file in all steps

--- oeml/oeml.sh
+++ oeml/oeml.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python2 genFeatsP.py -i ../../../dbpedia-dump-clean/ -o ./ -n 3 -t http://dbpedia.org/resource/Category:Contents -s http://www.w3.org/2004/02/skos/core#broader -m http://purl.org/dc/terms/subject -f nt ; Rscript identity-aggregator-p.R *.csv training.csv ; ./weka-batch.sh training.csv results/dbpedia-dump-clean.txt 2> error.txt
+python2 genFeatsP.py -i ../../../dbpedia-dump-clean/ -o ./ -n 3 -t http://dbpedia.org/resource/Category:Contents -s http://www.w3.org/2004/02/skos/core#broader -m http://purl.org/dc/terms/subject -f nt 2> error.txt; Rscript identity-aggregator-p.R *.csv training.csv 2>> error.txt; ./weka-batch.sh training.csv results/dbpedia-dump-clean.txt 2>> error.txt
