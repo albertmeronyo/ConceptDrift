1ec178e1ed12b3bba1bb173916d9b9212aa20d2d
Albert Meronyo-Penyuela <albert.meronyo@gmail.com>
2014-04-15 11:00:13 +0200

    Converted parameters to input arguments

--- oeml/oeml.sh
+++ oeml/oeml.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python2 genFeatsP.py -i ../../../dbpedia-dump-clean/ -o ./ -n 3 -t http://dbpedia.org/resource/Category:Contents -s http://www.w3.org/2004/02/skos/core#broader -m http://purl.org/dc/terms/subject -f nt 2> error.txt; Rscript identity-aggregator-p.R *.csv training.csv 2>> error.txt; ./weka-batch.sh training.csv results/dbpedia-dump-clean.txt 2>> error.txt
+DATA_DIR=$1
+EXP_DIR=$2
+NUM_SNAPS=$3
+
+python2 genFeatsP.py -i DATA_DIR -o EXP_DIR -n NUM_SNAPS -t http://dbpedia.org/resource/Category:Contents -s http://www.w3.org/2004/02/skos/core#broader -m http://purl.org/dc/terms/subject -f nt 2> error.txt; Rscript identity-aggregator-p.R EXP_DIR/*.csv EXP_DIR/training.csv 2>> error.txt; ./weka-batch.sh EXP_DIR/training.csv EXP_DIR/results.txt 2>> error.txt
