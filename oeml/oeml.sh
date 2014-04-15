30843684913d18865de646338001be540faa159f
Albert Meronyo-Penyuela <albert.meronyo@gmail.com>
2014-04-15 15:11:19 +0200

    Top, sub and in props as arguments too

--- oeml/oeml.sh
+++ oeml/oeml.sh
@@ -3,7 +3,10 @@
 DATA_DIR=$1
 EXP_DIR=$2
 NUM_SNAPS=$3
+TOP_CAT=$4
+SUB_PROP=$5
+IN_PROP=$6
 
-python2 genFeatsP.py -i $DATA_DIR -o $EXP_DIR -n $NUM_SNAPS -t http://dbpedia.org/resource/Category:Contents -s http://www.w3.org/2004/02/skos/core#broader -m http://purl.org/dc/terms/subject -f nt 2> $EXP_DIR/error.txt
+python2 genFeatsP.py -i $DATA_DIR -o $EXP_DIR -n $NUM_SNAPS -t $TOP_CAT -s $SUB_PROP -m $IN_PROP -f nt 2> $EXP_DIR/error.txt
 Rscript identity-aggregator-p.R $EXP_DIR/*.csv $EXP_DIR/training.csv 2>> $EXP_DIR/error.txt
 ./weka-batch.sh $EXP_DIR/training.csv $EXP_DIR/results.txt 2>> $EXP_DIR/error.txt
