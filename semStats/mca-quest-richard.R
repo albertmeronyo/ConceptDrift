60f17950af4c807e3b4781dc9b91ee107b02cc99
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2014-04-15 17:22:01 +0200

    Experiments for occupation female drift for Richard

---
+++ semStats/mca-quest-richard.R
@@ -0,0 +1,32 @@
+require(gdata)
+require(FactoMineR)
+require(MASS)
+require(ggplot2)
+
+###########################
+# Load CSV with SPARQL dump
+###########################
+res <- read.csv('/Users/Albert/src/ConceptDrift/semStats/data/occupations-overijssel.csv', header = TRUE)
+a <- res
+
+################################
+# Removal of column "population"
+################################
+
+# We use the population column to repeat the rows as many times as this column specifies
+# Then we remove the column
+a <- a[rep(1:nrow(a), a$pop),]
+a$pop <- NULL
+
+#####
+# MCA
+#####
+
+cats_a <- apply(a, 2, function(x) nlevels(as.factor(x)))
+
+# MCA of the MASS package
+mca_a <- mca(a, nf = 5)
+
+# Plots
+mca_a_vars_df <- data.frame(mca_a$cs, Variable = rep(names(cats_a), cats_a))
+ggplot(data = mca_a_vars_df, aes(x = X1, y = X2, label = rownames(mca_a_vars_df))) + geom_hline(yintercept = 0, colour = "gray70") + geom_vline(xintercept = 0, colour = "gray70") + geom_text(aes(colour = Variable)) + ggtitle("BRT-1889 dataset in Overijssel")
