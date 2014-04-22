a40afc8d90f91e92c4eb13d1f90756fbe7a79ef1
Albert Meronyo-Penyuela <albert.meronyo@gmail.com>
2014-04-22 16:18:36 +0200

    Cleaned deprecated code

--- oeml/identity-aggregator-p.R
+++ oeml/identity-aggregator-p.R
@@ -3,8 +3,6 @@
 ##########################################################
 
 args <- commandArgs(trailingOnly = TRUE)
-#setwd("/Users/Albert/src/ConceptDrift/oeml")
-#args <- c("data/feats-change_3.5.1.csv", "data/feats-change_3.6.csv", "data/feats-change_3.7.csv", "data/feats-change_3.9.csv", "baz.csv")
 
 input.files <- head(args,-2)
 output.file <- tail(args,1)
@@ -62,14 +60,6 @@ merged[,1] <- NULL
 
 # Rename features
 colnames(merged) <- as.character(seq(1:length(colnames(merged))))
-#colnames(merged) <- c('resource', 
-#                      'children0.0', 'children1.0', 'children2.0', 'children3.0', 'parents.0', 'siblings.0', 'members.0', 'membersChildren0.0', 'membersChildren1.0', 'membersChildren2.0', 'membersChildren3.0','ratio.0','ratio0.0','ratio1.0','ratio2.0','ratio3.0',
-#                      'children0.1', 'children1.1', 'children2.1', 'children3.1', 'parents.1', 'siblings.1', 'members.1', 'membersChildren0.1', 'membersChildren1.1', 'membersChildren2.1', 'membersChildren3.1','ratio.1','ratio0.1','ratio1.1','ratio2.1','ratio3.1',
-#                      'children0.2', 'children1.2', 'children2.2', 'children3.2', 'parents.2', 'siblings.2', 'members.2', 'membersChildren0.2', 'membersChildren1.2', 'membersChildren2.2', 'membersChildren3.2','ratio.2','ratio0.2','ratio1.2','ratio2.2','ratio3.2',
-#                      'extended')
-
-# Quote everything
-# merged <- data.frame(lapply(merged, as.character), stringsAsFactors=FALSE)
 
 # Save dataset
 write.csv(merged, output.file, quote = TRUE, na = "", row.names = FALSE)
