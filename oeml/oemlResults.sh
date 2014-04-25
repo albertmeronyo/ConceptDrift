cca589cc5ec5ad8ead3a2bd7c67e614a82b4b16c
Albert Meronyo-Penyuela <albert.meronyo@gmail.com>
2014-04-25 10:19:50 +0200

    Fix #41

---
+++ oeml/oemlResults.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Filter OEML results from exp/*/results.txt
+
+EXPDIR="./exp/"
+
+for f in `ls $EXPDIR`
+do
+    echo 'EXPERIMENT '$f;
+    cat $EXPDIR$f/results.txt | grep "Weighted\|weka\|Precision";
+    printf '\n\n';
+done
