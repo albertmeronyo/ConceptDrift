4a135ec828c85ab73c05c418acdc542d8f16aaeb
Albert Meronyo-Penyuela <albert.meronyo@gmail.com>
2014-04-28 18:35:12 +0200

    Fix #45

---
+++ oeml/oemlTest.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Execute one test OEML experiment
+
+./oeml.sh ../../../cedar-brt-drift/ ./exp/test/ 3 1 1 novelChildren T http://cedar.example.org/ns#hisco http://www.w3.org/2004/02/skos/core#broader http://cedar.example.org/ns#occupation &
