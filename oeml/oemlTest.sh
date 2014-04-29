f6f8c954d9a167de7d004acbfc6352acfac7bde1
Albert Meronyo-Penyuela <albert.meronyo@gmail.com>
2014-04-29 14:41:20 +0200

    Removing background execution

--- oeml/oemlTest.sh
+++ oeml/oemlTest.sh
@@ -2,6 +2,7 @@
 
 # Execute one test OEML experiment
 
-./oeml.sh ../../../cedar-brt-drift/ ./exp/test/ 3 1 1 novelChildren T http://cedar.example.org/ns#hisco http://www.w3.org/2004/02/skos/core#broader http://cedar.example.org/ns#occupation &
+mkdir ./exp/test/ 2> /dev/null
+./oeml.sh ../../../cedar-brt-drift/ ./exp/test/ 3 1 1 novelChildren T http://cedar.example.org/ns#hisco http://www.w3.org/2004/02/skos/core#broader http://cedar.example.org/ns#occupation
 
 exit 0
