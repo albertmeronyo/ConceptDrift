0a6a74b1ee12c804f4277762ebb1be88a017e499
Albert Meronyo-Penyuela <albert.meronyo@gmail.com>
2014-05-07 16:44:22 +0200

    Testing with last  CEDAR exp for the paper

--- oeml/oemlTest.sh
+++ oeml/oemlTest.sh
@@ -3,7 +3,7 @@
 # Execute one test OEML experiment
 
 mkdir ./exp/test/ 2> /dev/null
-#./oeml.sh ../../../cedar-brt-drift/ ./exp/test/ 3 1 1 allDrift T http://cedar.example.org/ns#hisco http://www.w3.org/2004/02/skos/core#broader http://cedar.example.org/ns#occupation
-./oeml.sh ../../../dbpedia-ontology/ ./exp/test/ 5 1 1 allDrift T http://www.w3.org/2002/07/owl#Thing http://www.w3.org/2000/01/rdf-schema#subClassOf http://www.w3.org/1999/02/22-rdf-syntax-ns#type http://www.w3.org/2000/01/rdf-schema#label
+./oeml.sh ../../../cedar-brt-drift/ ./exp/test/ 8 1 1 allDrift T http://cedar.example.org/ns#hisco http://www.w3.org/2004/02/skos/core#broader http://cedar.example.org/ns#occupation http://www.w3.org/2004/02/skos/core#prefLabel
+#./oeml.sh ../../../dbpedia-ontology/ ./exp/test/ 5 1 1 allDrift T http://www.w3.org/2002/07/owl#Thing http://www.w3.org/2000/01/rdf-schema#subClassOf http://www.w3.org/1999/02/22-rdf-syntax-ns#type http://www.w3.org/2000/01/rdf-schema#label
 
 exit 0
