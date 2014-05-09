221c6058bd474e8a8eea4376811f478f91b00128
Albert Meronyo-Penyuela <albert.meronyo@gmail.com>
2014-05-09 11:12:36 +0200

    No partial DFS for DBO

--- oeml/oemlExperiments-dbo.sh
+++ oeml/oemlExperiments-dbo.sh
@@ -39,7 +39,7 @@ do
     do
 	mkdir $EXP_DIRECTORY$NAME-$N-1-1-$CD-$IAS/ 2> /dev/null
 	rm -rf $EXP_DIRECTORY$NAME-$N-1-1-$CD-$IAS/* 2> /dev/null
-	./oeml.sh $INPUT_DATA $EXP_DIRECTORY$NAME-$N-1-1-$CD-$IAS/ $N 1 1 $CD $IAS $TOP $SUB_PROP $MEMB_PROP $LABEL_PROP -pt
+	./oeml.sh $INPUT_DATA $EXP_DIRECTORY$NAME-$N-1-1-$CD-$IAS/ $N 1 1 $CD $IAS $TOP $SUB_PROP $MEMB_PROP $LABEL_PROP
     done
 done
 
