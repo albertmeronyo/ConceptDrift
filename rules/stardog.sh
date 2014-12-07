686cc965846017e7a8167b0f6ea0e1c501bd2013
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2014-12-07 10:44:12 +0100

    trying to use stardog reasoning explanations

--- rules/stardog.sh
+++ rules/stardog.sh
@@ -8,14 +8,14 @@ R_BOX="/Users/Albert/src/ConceptDrift/rules/people-rules.ttl"
 PATH=$PATH:$HOME_STARDOG
 
 # Backend server
-stardog-admin server start
+# stardog-admin server start
 
 # Populate knowledge base
 stardog-admin db drop $DB_NAME
 stardog-admin db create -n $DB_NAME $A_BOX $R_BOX
 
 # Querying
-./stardog query "people" "select ?s ?o where {?s <http://example.org/ns#edits> ?o}"
+# ./stardog query "people" "select ?s ?o where {?s <http://example.org/ns#edits> ?o}"
 
 # With SL inference! :-)
-./stardog query "people;reasoning=SL" "select ?s ?o where {?s <http://example.org/ns#edits> ?o}"
+# ./stardog query "people;reasoning=SL" "select ?s ?o where {?s <http://example.org/ns#edits> ?o}"
