f2f90dcb565c371357cb989b7ec684ccf295f4a2
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2015-01-05 10:08:05 +0100

    syntax error in people-rules

--- rules/src/push-ler.sql
+++ rules/src/push-ler.sql
@@ -1,4 +1,4 @@
 SPARQL CLEAR GRAPH <urn:graph:ler:rules:people>;
-ld_dir('/home/virtuoso/triples2load/', 'people-rules.ttl', 'urn:graph:ler:rules:people');
+ld_dir('/home/amp/src/ConceptDrift/rules/data', 'people-rules.ttl', 'urn:graph:ler:rules:people');
 rdf_loader_run();
 checkpoint;
