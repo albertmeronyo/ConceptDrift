ab57e4c27cb961b42a296612d14fe85d9b51c189
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2015-01-05 10:48:50 +0100

    user and graph permissions

--- rules/src/push-ler.sql
+++ rules/src/push-ler.sql
@@ -1,3 +1,8 @@
+DB.DBA.USER_CREATE ('ler', sprintf('%d',rnd(1e16)));
+GRANT SPARQL_SELECT to "ler";
+DB.DBA.RDF_DEFAULT_USER_PERMS_SET ('ler', 0);
+DB.DBA.RDF_GRAPH_USER_PERMS_SET ('urn:graph:ler:rules:people', 'ler', 1);
+
 SPARQL CLEAR GRAPH <urn:graph:ler:rules:people>;
 ld_dir('/home/amp/src/ConceptDrift/rules/data', 'people-rules.ttl', 'urn:graph:ler:rules:people');
 rdf_loader_run();
