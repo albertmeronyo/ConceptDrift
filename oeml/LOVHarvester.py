a5fcca5b41df78b4bd306a5b06498327bf94c412
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2015-01-30 18:10:21 +0100

    Logging

--- oeml/LOVHarvester.py
+++ oeml/LOVHarvester.py
@@ -5,18 +5,27 @@
 
 import urllib2
 import json
+import argparse
+import logging
 
 class LOVHarvester():
     LOV_LIST = "http://lov.okfn.org/dataset/lov/api/v2/vocabulary/list"
     LOV_DETAIL = "http://lov.okfn.org/dataset/lov/api/v2/vocabulary/info?vocab="
 
-    def __init__(self):
+    def __init__(self, __logLevel):
         '''
         Class constructor
         '''
+        self.log = logging.getLogger('LOVHarvester')
+        self.log.setLevel(__logLevel)
+
+        self.log.info("Initializing data structures...")
         self.prefixes = []
 
+        self.log.info("Getting prefixes...")
         self.getPrefixes()
+
+        self.log.info("All done.")
 
     def getPrefixes(self):
         '''
@@ -28,5 +37,22 @@ class LOVHarvester():
             self.prefixes.append(r["prefix"])
 
 if __name__ == "__main__":
-    l = LOVHarvester()
+    # Argument parsing
+    parser = argparse.ArgumentParser(description="Retrieves versions of Linked Open Vocabularies from lov.okfn.org")
+    parser.add_argument('--verbose', '-v',
+                        help = "Be verbose -- debug logging level",
+                        required = False, 
+                        action = 'store_true')
+    args = parser.parse_args()
+
+    # Logging
+    logLevel = logging.INFO
+    if args.verbose:
+        logLevel = logging.DEBUG
+    logging.basicConfig(level=logLevel)
+    logging.info("Initializing...")
+
+    # Instance
+    l = LOVHarvester(logLevel)
+    logging.info("Exiting...")
     exit(0)
